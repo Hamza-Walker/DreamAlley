49e25601ba86e3e366a56a023acadcd9219e7059
hamza walker <hamzawalker.vienna@gmail.com>
2024-01-23 23:21:21 +0100

    due to an issue with starting the project with T3 create app i decided to just create nextjs app from scratch and add the librabrys one after the other. so far set up the .env file and created the drizzle.config file and schema directory and file.

---
+++ src/db/schema/index.ts
@@ -0,0 +1,10 @@
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+
+export const posts = pgTable ('posts', {
+ 	id:uuid('id').defaultRandom().primaryKey(),
+	title:varchar('title', {length:256}).notNull(),
+	content:varchar('content',{length:256}).notNull(),
+	createdAt:timestamp('created_at').defaultNow().notNull(),
+})
+export type Post = InferSelectModel<typeof posts>;
+export type NewPost = InferInsertModel<typeof posts>; 
