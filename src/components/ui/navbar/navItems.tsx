8206d26caa9c3b4097083ff35c34abcf88dec492
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-06 22:32:23 +0200

    removed items from the navbar

--- src/components/ui/navbar/navItems.tsx
+++ src/components/ui/navbar/navItems.tsx
@@ -1,61 +1,43 @@
-import { NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuTrigger } from './Navigation-menu';
+import {
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuTrigger,
+} from "./Navigation-menu";
 
-import React from 'react';
-import s from './Navbar.module.css';
+import React from "react";
+import s from "./Navbar.module.css";
 
 // components/navItems.tsx
 
 export const navItems = [
-  {
-    id: 'home',
-    label: 'Home',
-    href: '/',
-  },
-  {
-    id: 'about',
-    label: 'About',
-    href: '/about',
-  },
-  {
-    id: 'shop',
-    label: 'Shop',
-    href: '/shop',
-  },
-  {
-    id: 'artists',
-    label: 'Artists',
-    href: '/artists',
-  },
-  {
-    id: 'gallery',
-    label: 'Gallery',
-    href: '/gallery',
-  },
-  {
-    id: 'events',
-    label: 'Events',
-    href: '/events',
-  },
-  {
-    id: 'community',
-    label: 'Community',
-    href: '/community',
-  },
-  {
-    id: 'contact',
-    label: 'Contact',
-    href: '/contact',
-  },
-  // Add more items as needed
+	{
+		id: "home",
+		label: "Home",
+		href: "/",
+	},
+	{
+		id: "about",
+		label: "About",
+		href: "/about",
+	},
+	{
+		id: "shop",
+		label: "Shop",
+		href: "/shop",
+	},
+
+	// Add more items as needed
 ];
 
 export const renderNavItems = () => {
-  return navItems.map((item) => (
-    <NavigationMenuItem key={item.id} className={s.navItem}>
-      <NavigationMenuLink href={item.href} className={s.link}>
-        <NavigationMenuTrigger className={s.trigger}>{item.label}</NavigationMenuTrigger>
-      </NavigationMenuLink>
-    </NavigationMenuItem>
-  ));
+	return navItems.map((item) => (
+		<NavigationMenuItem key={item.id} className={s.navItem}>
+			<NavigationMenuLink href={item.href} className={s.link}>
+				<NavigationMenuTrigger className={s.trigger}>
+					{item.label}
+				</NavigationMenuTrigger>
+			</NavigationMenuLink>
+		</NavigationMenuItem>
+	));
 };
-
